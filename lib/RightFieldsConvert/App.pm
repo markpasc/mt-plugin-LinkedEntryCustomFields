cd1df495f17e3fdd1d222e361410eaed4b85fd1c
Mark Paschal <markpasc@markpasc.org>
2008-07-07 20:51:43 +0000

    Scaffold some app methods

---
+++ lib/RightFieldsConvert/App.pm
@@ -0,0 +1,17 @@
+
+package RightFieldsConvert::App;
+
+sub list_entries_mini {
+    my $app = shift;
+}
+
+sub select_entry {
+    my $app = shift;
+}
+
+sub convert_rf2cf {
+    my $app = shift;
+}
+
+1;
+
