601695b1a17627254e214e5b4be9e93e3ac423a9
Mark Paschal <markpasc@markpasc.org>
2008-07-14 16:47:15 +0000

    First go at a LECF converter

--- plugins/LinkedEntryCustomFields/lib/LinkedEntryCustomFields/App.pm
+++ plugins/LinkedEntryCustomFields/lib/LinkedEntryCustomFields/App.pm
@@ -187,5 +187,132 @@ sub load_customfield_tags {
     return {};
 }
 
+sub convert_rf2cf {
+    my $app = shift;
+
+    # Look for RF field definitions.
+    my $def_iter = MT->model('plugindata')->load_iter({ plugin => 'rightfields' });
+    
+    my (@tags, @fields);
+    DEF: while (my $def = $def_iter->()) {
+        # Don't care about default settings, only the ones actually in use on blogs.
+        next DEF if $def->key !~ m{ \A blog_ }xms;
+        push @tags,   $def->clone if $def->key =~ m{ _tags \z }xms;
+        push @fields, $def->clone if $def->key =~ m{ _extra \z }xms;
+    }
+
+    my %tags_for_fields;
+    TAG: for my $tag_def (@tags) {
+        MT->log('OH HAI from tag ' . $tag_def->key);
+        $tag_def->key =~ m{ \A blog_(\d+) }xms or next TAG;
+        my $blog_id = $1;
+
+        # We don't know from the tag data what fields are entries, so record all of them.
+        my $tags_data = $tag_def->data;
+        for my $tag_data (@$tags_data) {
+            my ($field, $tag_name) = @{$tag_data}{qw( field tag )};
+
+            # Each CF can have only one tag, and we'll need to pull them out by field.
+            $tags_for_fields{$blog_id}->{$field} = $tag_name;
+        }
+    }
+    
+    my (%fields_for_blog, %fields_by_id, %datasource_for_blog);
+    FIELDS: for my $fields_def (@fields) {
+        MT->log('OH HAI from fieldset ' . $fields_def->key);
+        $fields_def->key =~ m{ \A blog_(\d+) }xms or next FIELDS;
+        my $blog_id = $1;
+        
+
+            if $fields_def->data
+        
+        my $fields_data = $fields_def->data;
+        $datasource_for_blog{$blog_id} = $fields_data->{datasource};
+        my $fields = $fields_data->{cols};
+        FIELD: while (my ($field_id, $field_data) = each %$fields) {
+            # Entry fields only.
+            next FIELD if !$field_data->{type}
+                || $field_data->{type} ne 'entry';
+
+            my %field;
+            $field{$_} = $field_data->{$_} for qw( label weblog category_ids );
+            $field{blog_id} = $blog_id;
+            $field{tag} = $tags_for_fields{$blog_id}->{$field_id};
+            
+            $fields_for_blog{$blog_id} ->{$field_id} = \%field;
+            $fields_by_id   {$field_id}->{$blog_id}  = \%field;
+        }
+    }
+    
+    # Upgrade duplicates to global fields.
+    FIELD_BY_ID: while (my ($field_id, $fields) = each %fields_by_id) {
+        # Leave fields that are only in one blog alone.
+        next FIELD_BY_ID if 1 == scalar keys %$fields;
+        
+        my ($first_field, @fields) = values %$fields;
+        for my $datum (qw( label weblog category_ids tag )) {
+            my $first_value = $first_field->{$datum};
+            for my $next_field (@fields) {
+                my $next_value = $next_field->{$datum};
+                next FIELD_BY_ID if  defined $first_value && !defined $next_value;
+                next FIELD_BY_ID if !defined $first_value &&  defined $next_value;
+                next FIELD_BY_ID if  defined $first_value
+                    && $first_value ne $next_value;
+            }
+        }
+        
+        # Huh, everything matched. Make this a global field.
+        # TODO: Delete the value from each of their %fields_by_blog groups, using blog_id member.
+        # TODO: Assign first_field to blog_id=0 group. Or have a separate set of global fields?
+    }
+    
+    # Make corresponding custom fields.
+    while (my ($blog_id, $fields) = each %fields_for_blog) {
+        while (my ($field_id, $field_data) = each %$fields) {
+            # Make or update the corresponding custom field.
+            my $cf = MT->model('field')->load({
+                blog_id  => $blog_id,
+                basename => $field_id,
+            });
+            $cf ||= MT->model('field')->new;
+            
+            my $options = $field_data->{weblog};
+            $options = join q{,}, $field_data->{category_ids}
+                if $field_data->{category_ids};
+
+            $cf->set_values({
+                name     => $field_data->{label},
+                obj_type => 'entry',
+                type     => 'entry',
+                options  => $options,
+                basename => $field_id,
+            });
+            $cf->blog_id($blog_id) if $blog_id;  # TODO: 0 = global?
+
+            my $tag = $field_data->{tag};
+            $tag ||= lc join q{}, $cf->obj_type, 'data', $cf->name;
+            # TODO: ensure the tag is unique, since CF makes us.
+            $cf->tag($tag);
+
+            $cf->save or die $cf->errstr;
+            
+            # Copy the data for that field.
+            my $datasource = $datasource_for_blog{$blog_id};
+            if ($datasource eq '_pseudo') {
+                # omg pseudo objects aiee
+            }
+            else {
+                my $sql = MT::ObjectDriver::SQL->new;
+                $sql->
+            }
+        }
+    }
+    
+    # Start copying data.
+    
+    
+    return $app->return_to_dashboard( redirect => 1 );
+}
+
 1;
 
